ddce4eeded3da58a1d236914b04a1fca93355c46
abhay tiwari <tiwariabhay1085@gmail.com>
2026-04-07 17:09:01 +0530

    filter option add

--- server/controllers/foodControllers.js
+++ server/controllers/foodControllers.js
@@ -1,13 +1,42 @@
 const food = require('../models/food')
 
 //Get all food
-const getFoods = async (req,res)=>{
-    try {
-        const foods = await food.find();
-        res.json(foods);
-    } catch (error) {
-        res.status(500).json({message:error.message});
+const getFoods = async (req, res) => {
+  try {
+    const { search, category, page = 1, limit = 5, sort } = req.query;
+
+    let query = {};
+
+    if (search) {
+      query.name = { $regex: search, $options: "i" };
     }
+
+    if (category) {
+      query.category = category;
+    }
+
+    let sortOption = {};
+
+    if (sort === "low") sortOption.price = 1;
+    if (sort === "high") sortOption.price = -1;
+
+    const foods = await food.find(query)
+      .sort(sortOption)
+      .skip((page - 1) * limit)
+      .limit(Number(limit));
+
+    const total = await food.countDocuments(query);
+
+    res.json({
+      total,
+      page: Number(page),
+      pages: Math.ceil(total / limit),
+      foods
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 //Add food
